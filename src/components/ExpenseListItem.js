98f0d04dd6130e4802016c8158603ee443ff2779
Stephanie <steffik02@gmail.com>
2018-07-15 16:01:08 -0400

    Added currency and date formatting

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import moment from 'moment';
+import numeral from 'numeral';
 
 //connect component to access dispatch
 //import correct delete functin
@@ -10,7 +12,7 @@ export default ({id, description, amount, createdAt}) => (
         <Link to={'/edit/' + id}>
             <h3>Description: {description}</h3>
         </Link>
-        <p>Amount: ${amount/100}</p>
-        <p>Date Created: {createdAt}</p>
+        <p>Amount: {numeral(amount / 100).format('$0,0.00')}</p>
+        <p>Date Due: {moment(createdAt).format('MMMM D, YYYY')}</p>
     </div>
 );
