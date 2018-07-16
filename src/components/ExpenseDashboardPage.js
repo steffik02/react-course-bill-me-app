7b5a4e93be356559882801797921d44efae25cda
Stephanie <steffik02@gmail.com>
2018-07-16 13:33:03 -0400

    Added a summary of visible expenses

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
+import ExpensesSummary from './ExpensesSummary';
 
 const ExpenseDashboardPage = () => (
     <div>
         This dashboard will help you steer your expense spaceship into financial freedom.
+        <ExpensesSummary />
         <ExpenseListFilters />
         <ExpenseList />
     </div>
